bf8c9eeeb2ab5afcaca4a6715f592e8feb82ceb0
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2023-03-02 02:24:58 +0530

    footer_added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-        <Navbar />
-    </div>
+    <Footer/>
   );
 }
 
