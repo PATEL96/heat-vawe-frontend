d61d1825d022fc1445340c84651ef96d9baec9e4
PATEL96 <patel.raj.official.96@gmail.com>
2023-03-02 17:15:20 +0530

    feat: Added Sticky Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import './Navbar.css';
 import logo from '../images/logo.png';
 
 export default function Navbar() {
+	const[navbar, setNavbar] = useState(false);
+
+	var viewport_height = window.innerHeight;
+
+	const changeNav = () => {
+		if(window.scrollY >= (viewport_height*0.1)){
+			setNavbar(true);
+		} else {
+			setNavbar(false);
+		}
+	}
+
+	window.addEventListener('scroll', changeNav);
+
 	return(
-		<div className="nav-main">
+		<div className={navbar ? 'nav-main active' : 'nav-main'}>
 			<div className="navContent">
 				<img src={logo} alt="Heat_Vawe" className="Logo" />
 				<div className="options" >
-					<ul>
-						<li>
-							Home
-						</li>
-						<li>
-							T-Shirts
-						</li>
-						<li>
-							About
-						</li>
-					</ul>
+					<div>Home</div>
+					<div>T-Shirts</div>
+					<div>About</div>
 				</div>
 			</div>
 		</div>
