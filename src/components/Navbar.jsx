a71165f1b44b27aa09bb00eed0c43bc6b17957b4
PATEL96 <patel.raj.official.96@gmail.com>
2023-03-02 10:18:21 +0530

    feat: added minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,21 @@ import logo from '../images/logo.png';
 export default function Navbar() {
 	return(
 		<div className="nav-main">
-			<div>
+			<div className="navContent">
 				<img src={logo} alt="Heat_Vawe" className="Logo" />
+				<div className="options" >
+					<ul>
+						<li>
+							Home
+						</li>
+						<li>
+							T-Shirts
+						</li>
+						<li>
+							About
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
