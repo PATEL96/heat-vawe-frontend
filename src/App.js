b61c3e46b93f369f05f9ca61eb00897c40ee7ce8
Sirjandeep Singh Kandhari <72351791+KingSlayer-KS@users.noreply.github.com>
2023-03-03 00:27:02 +0530

    Listed_items_added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import ProfuctsListed from "./components/ProductsListed";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <ProfuctsListed/>
       <Footer/>
     </div>
   );
