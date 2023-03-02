65fc9715d45d86055f786b6d532b105834538a0b
PATEL96 <patel.raj.official.96@gmail.com>
2023-03-02 09:58:58 +0530

    feat: Added Some Changes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import './Navbar.css';
+import logo from '../images/logo.png';
 
 export default function Navbar() {
 	return(
 		<div className="nav-main">
-			This is a Navbar.
+			<div>
+				<img src={logo} alt="Heat_Vawe" className="Logo" />
+			</div>
 		</div>
 	);
 }
-
-// const Navbar = () => {
-// 	return (
-// 		<div>THis is A Navbar.</div>
-// 	)
-// }
-
-// export default Navbar;
