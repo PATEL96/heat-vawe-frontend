0f3b20539935a0b759ab266007f3125da302be06
PATEL96 <patel.raj.official.96@gmail.com>
2023-03-11 04:02:42 +0530

    feat: Strated Some Responsive Navs.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,18 @@ import logoImg from '../images/logo.png';
 
 export default function Navbar() {
 	const[navbar, setNavbar] = useState(false);
+	const[navContent, setNavConent] = useState(false);
+	const [isMobile, setIsMobile] = useState(true);
 
 	var viewport_height = window.innerHeight;
 
 	const changeNav = () => {
 		if(window.scrollY >= (viewport_height*0.04)){
 			setNavbar(true);
+			setNavConent(true);
 		} else {
 			setNavbar(false);
+			setNavConent(false);
 		}
 	}
 
@@ -20,26 +24,31 @@ export default function Navbar() {
 
 	return(
 		<div className={navbar ? 'nav-main active' : 'nav-main'}>
-			<div className="navContent">
+			<div className={navContent ? 'navContent active' : 'navContent'}>
 				<div className="leftSide">
 					<img src={logoImg} alt="Heat_Vawe" className="Logo" />
-					<div className="home">
-						Home
-						<hr color="none" />
+					<div className={isMobile ? 'list-desktop' : 'list-mobile'} onClick={() => setIsMobile(true)} >
+						<div className="home">
+							Home
+							<hr color="none" />
+						</div>
+						<div className="over">
+							Oversized T-Shirts
+							<hr color="none" />
+						</div>
+						<div className="collection">
+							{/* // Todo: make list Here */}
+							Collections
+							<hr color="none" />
+						</div>
+						<div className="about">
+							About
+							<hr color="none" />
+						</div>
 					</div>
-					<div className="over">
-						Oversized T-Shirts
-						<hr color="none" />
-					</div>
-					<div className="collection">
-						{/* // Todo: make list Here */}
-						Collections
-						<hr color="none" />
-					</div>
-					<div className="about">
-						About
-						<hr color="none" />
-					</div>
+					<button className="mobile-menu" onClick={() => setIsMobile(!isMobile)}>
+						{isMobile ? (<i className="fa-solid fa-bars" />) : (<i className="fa-solid fa-times" />)}
+					</button>
 				</div>
 			</div>
 		</div>
